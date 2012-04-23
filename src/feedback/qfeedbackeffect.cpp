57b305b4a39620dd54c169fff33ba22ff742c71f
Harri Heine <harri.heine@nokia.com>
2012-04-23 09:23:09 +0300

    Changing Fileeffects as internal
    Change-Id: If9b8c9941e6261ba73b989ba327147b4ab409dd6
    Reviewed-by: Jaakko Korpela <jaakko.korpela@nokia.com>

--- src/feedback/qfeedbackeffect.cpp
+++ src/feedback/qfeedbackeffect.cpp
@@ -601,6 +601,7 @@ QFeedbackEffect::State QFeedbackHapticsEffect::state() const
 }
 
 /*!
+\internal
     \class QFeedbackFileEffect
     \ingroup feedback
     \inmodule QtFeedback
@@ -658,6 +659,7 @@ void QFeedbackFileEffectPrivate::loadFinished(bool success)
 
 
 /*!
+    \internal
     Constructs the QFeedbackFileEffect class, and passes \a parent to
     QObject's constructor.
 */
@@ -666,6 +668,7 @@ QFeedbackFileEffect::QFeedbackFileEffect(QObject *parent) : QFeedbackEffect(pare
 }
 
 /*!
+    \internal
     Stops the feedback and unloads the file if necessary.
 */
 QFeedbackFileEffect::~QFeedbackFileEffect()
@@ -682,6 +685,7 @@ int QFeedbackFileEffect::duration() const
 }
 
 /*!
+    \internal
     \property QFeedbackFileEffect::source
     \brief the url of the file that is loaded.
 
@@ -709,6 +713,7 @@ void QFeedbackFileEffect::setSource(const QUrl &source)
 }
 
 /*!
+    \internal
     \property QFeedbackFileEffect::loaded
     \brief reports if the file has been successfully loaded.
 */
@@ -731,6 +736,7 @@ void QFeedbackFileEffect::setLoaded(bool load)
 
 
 /*!
+    \internal
     \fn void QFeedbackFileEffect::load()
 
     Makes sure that the file associated with the feedback object is loaded.
@@ -743,6 +749,7 @@ void QFeedbackFileEffect::load()
 }
 
 /*!
+    \internal
     \fn void QFeedbackFileEffect::unload()
 
     makes sure that the file associated with the feedback object is unloaded.
@@ -756,6 +763,7 @@ void QFeedbackFileEffect::unload()
 
 
 /*!
+    \internal
     \fn QStringList QFeedbackFileEffect::supportedMimeTypes()
 
     returns the MIME types supported for playing effects from file.
